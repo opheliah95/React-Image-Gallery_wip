f6f3a62b88dae324ea9137fcc64cf36dedfa1e8b
opheliah95 <opheliah95@gmail.com>
2022-04-12 12:03:57 +0100

    feat: reset search input after press submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,40 @@ import Header from "./components/Header";
 import Search from "./components/Search";
 import { useState } from "react";
 
-const UNSPLASH_KEY = REACT_APP_UNSPLASH_ACCESS_KEY;
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 const UNSPLASH_SECRET = process.env.REACT_APP_UNSPLASH_SECRET;
+const UNSPLASH_RAND_PHOTO_URL = `${process.env.REACT_APP_API_URL}${process.env.REACT_APP_RAND_ENDPOINT}`;
 
 const App = () => {
   const [word, setWord] = useState("");
+  const UNSPLASH_RAND_QUERY_STRING = `${UNSPLASH_RAND_PHOTO_URL}?query=${word}&client_id=${UNSPLASH_KEY}`;
 
   const handleSearchSubmit = (e) => {
-    console.log(word);
+    // some debugging here
+    // console.log(word);
+    // console.log(UNSPLASH_RAND_QUERY_STRING);
+
+    // first api call to random image
+    fetch(UNSPLASH_RAND_QUERY_STRING)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(`the error is: ${err}`);
+      });
     e.preventDefault();
+
+     // set the word to empty string
+     setWord("");
   };
 
   console.log(`word is now: ${word}`);
+  // console.log(UNSPLASH_RAND_PHOTO_URL);
+
+ 
+
+
   return (
     <div>
       <Header title="Images Gallery" />
