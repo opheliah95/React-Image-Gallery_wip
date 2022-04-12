5b97b15038dc9bae8258b9a4e92a39da3d2304a4
opheliah95 <opheliah95@gmail.com>
2022-04-12 12:03:58 +0100

    added linters and formaters and formatted files

--- api/main.py
+++ api/main.py
@@ -9,30 +9,32 @@ app = Flask(__name__)
 CORS(app)
 # load in env variables
 load_dotenv(dotenv_path="./.env.local")
-UNSPLASH_URL = os.getenv('UNSPLASH_URL', '')
-UNSPLASH_KEY = os.getenv('UNSPLASH_KEY', '')
-DEBUG = bool(os.getenv('DEBUG',True))
+UNSPLASH_URL = os.getenv("UNSPLASH_URL", "")
+UNSPLASH_KEY = os.getenv("UNSPLASH_KEY", "")
+DEBUG = bool(os.getenv("DEBUG", True))
 
 # enable debug mode
-app.config["DEBUG"]=DEBUG
+app.config["DEBUG"] = DEBUG
+
 
 @app.route("/")
 def hello():
     return "Hello world"
 
+
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file that has an unsplash key")
 
+
 @app.route("/new-image")
 def get_image():
-    search_word = request.args.get('query')
-    payload = {
-        "query": search_word
+    search_word = request.args.get("query")
+    payload = {"query": search_word}
+    headers = {
+        "Authorization": f"Client-ID {UNSPLASH_KEY}",
+        "Accept-Version": "v1",
     }
-    headers = {"Authorization": f"Client-ID {UNSPLASH_KEY}",
-               "Accept-Version": "v1",
-               }
-    
+
     response = get(url=UNSPLASH_URL, params=payload, headers=headers)
     data = response.json()
     # a list of parameters that will be interest to us
