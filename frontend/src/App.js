9079ebb995db516133850566c5bc00c0efc01541
opheliah95 <opheliah95@gmail.com>
2022-04-12 12:03:57 +0100

    env: added secret for unsplash api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,22 @@ import Header from "./components/Header";
 import Search from "./components/Search";
 import { useState } from "react";
 
+const UNSPLASH_KEY = REACT_APP_UNSPLASH_ACCESS_KEY;
+const UNSPLASH_SECRET = process.env.REACT_APP_UNSPLASH_SECRET;
 
 const App = () => {
-  const[word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
     console.log(word);
     e.preventDefault();
   };
-  console.log(`word is now: ${word}`)
+
+  console.log(`word is now: ${word}`);
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search word={word} setWord={setWord} queryEvent={handleSearchSubmit}/>
+      <Search word={word} setWord={setWord} queryEvent={handleSearchSubmit} />
     </div>
   );
 };
