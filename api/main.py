dc0048da7aad0e88e9a10d5bcbd570fb9815ecde
opheliah95 <opheliah95@gmail.com>
2022-04-12 12:03:57 +0100

    api: basic route setup

--- api/main.py
+++ api/main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hello world"
+
+@app.route("/new-image")
+def get_image():
+  search_word = request.args.get('query')
+  return {"word searched": search_word}
 
 if __name__ =="__main__":
   app.run(port=5050, host="0.0.0.0")
