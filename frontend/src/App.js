59ad8ba329e17bbb43637dbb86da8d0695c4e0ca
opheliah95 <opheliah95@gmail.com>
2022-04-12 12:03:57 +0100

    layout: fixed image layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
 import { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 const UNSPLASH_SECRET = process.env.REACT_APP_UNSPLASH_SECRET;
@@ -45,9 +46,15 @@ const App = () => {
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} queryEvent={handleSearchSubmit} />
-      {images.map((image, index) => (
-        <ImageCard image={image} key={index} />
-      ))}
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, index) => (
+            <Col key={index} className="pb-4">
+              <ImageCard image={image} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
