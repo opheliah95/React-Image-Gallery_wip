d017408116974677c211033cb6c00aa6642a443e
opheliah95 <opheliah95@gmail.com>
2022-07-04 15:26:20 +0100

    feat: changed protofoilo layout

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -7,6 +7,8 @@ const footer_style = {
 };
 
 const ImageCard = ({ image, deleteEvent, saveEvent }) => {
+  const author_name = image.user?.name;
+  const proto_url = image.user?.portfolio_url;
   return (
     <Card style={{ width: '18rem' }}>
       {image.urls.small && <Card.Img variant="top" src={image.urls.small} />}
@@ -17,23 +19,16 @@ const ImageCard = ({ image, deleteEvent, saveEvent }) => {
           Delete
         </Button>{' '}
         <Card.Footer style={footer_style} className="text-muted">
-          {image.user?.name ? (
-            <div>{`${image.user.name}`}</div>
-          ) : (
-            <div> No author name</div>
-          )}
-          {image.user?.portfolio_url ? (
+          {proto_url && (
             <Nav.Link
               href={image.user.portfolio_url}
               target="_blank"
               rel="noreferrer"
             >
-              {' '}
-              Portfolio
+              {author_name}
             </Nav.Link>
-          ) : (
-            <div></div>
           )}
+          {!proto_url && author_name}
         </Card.Footer>
         {!image.saved && (
           <Button variant="primary" onClick={() => saveEvent(image.id)}>
